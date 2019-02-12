cd8efa30100c5d4e3a14d4e4d6820454d00a130a
Duane Sibilly <duane@sibilly.com>
2019-02-12 15:27:10 -0500

    Use presage.parallel in atlasChatSend
    For the same reasons this was useful in atlasChatStart, using presage's parallel task runner here lets us collect all our results in one place at one time.

--- js/commands/atlasChatSend.js
+++ js/commands/atlasChatSend.js
@@ -2,6 +2,7 @@
 @module commands/atlasChatSend
 */
 import config from '../../Configuration';
+import presage from 'presage';
 import SRC from 'source-rcon-client';
 
 const atlasServer = config.atlas,
@@ -21,24 +22,55 @@ const atlasServer = config.atlas,
         process: (jinx, message, payload) => new Promise((resolve, reject) => {
             // Collect message metadata for reuse by command logging
             const author = message.author.tag,
-                authorId = message.author.id,
+                // authorId = message.author.id, // This isn't being used. What is it for?
                 channel = message.channel ?
                     message.channel.name :
                     null,
                 command = 'atlasChatSend',
+                rconCommand = `serverchat ${author}: ${payload}`,
                 server = message.guild ?
                     message.guild.name :
                     null;
 
-            atlasServer.ports.forEach(currentPort => {
-                // Create a new client for this host/port combination
-                const client = new SRC(atlasServer.host, currentPort, atlasServer.password),
-                    rconCommand = `serverchat ${message.author.tag}: ${message.content.slice(4)}} \n`;
+            presage.parallel(atlasServer.ports.reduce((portsMap, port) => {
+                if (!portsMap[port]) {
+                    portsMap[port] = () => new Promise(resolve => {
+                        const client = new SRC(atlasServer.host, port, atlasServer.password);
 
-                client.connect()
-                    .then(() => client.send(rconCommand))
-                    .then(response => {
-                        if (response !== 'Server received, But no response!! \n ') {
+                        client.connect().then(() => client.send(rconCommand)).then(response => {
+                            if (response === 'Server received, But no response!! \n ') {
+                                // Send was successful
+                                jinx._commandLog.command('atlasChatSendRelay', {
+                                    author,
+                                    channel,
+                                    command,
+                                    details: {
+                                        payload
+                                    },
+                                    message: message.content,
+                                    server
+                                });
+                            } else {
+                                jinx._commandLog.command('atlasChatSendError', {
+                                    author,
+                                    channel,
+                                    command,
+                                    details: {
+                                        payload
+                                    },
+                                    message: message.content,
+                                    server
+                                });
+                            }
+
+                            /*
+                            We always have to disconnect when we're done
+                            with a connection, success or failure.
+                            */
+                            return client.disconnect();
+                        }).then(() => {
+                            resolve('success');
+                        }).catch(error => {
                             jinx._commandLog.command('atlasChatSendError', {
                                 author,
                                 channel,
@@ -46,28 +78,42 @@ const atlasServer = config.atlas,
                                 details: {
                                     payload
                                 },
+                                error,
                                 message: message.content,
                                 server
                             });
-                            return;
-                        }
 
-                        // Send was successful
-                        jinx._commandLog.command('atlasChatSendRelay', {
-                            author,
-                            channel,
-                            command,
-                            details: {
-                                payload
-                            },
-                            message: message.content,
-                            server
+                            resolve('failure');
                         });
-                    }).catch(reject);
-            });
+                    });
+                }
 
-            // Regardless of what happens, fulfill the Promise so Jinx keeps going.
-            resolve();
+                return portsMap;
+            }, {})).then(results => {
+                jinx._commandLog.command('atlasChatSendFinish', {
+                    author,
+                    channel,
+                    command,
+                    details: {
+                        payload
+                    },
+                    results,
+                    server
+                });
+                resolve();
+            }).catch(error => {
+                jinx._commandLog.command('atlasPollError', {
+                    author,
+                    channel,
+                    command,
+                    details: {
+                        payload
+                    },
+                    error,
+                    server
+                });
+                reject(error);
+            });
         })
     };
 
